623ea7b2c40c849a59b2c24bf7762ff8e3ec3dce
Shreya <shreyasagar0918@gmail.com>
2026-03-17 19:41:36 -0400

    feat: add getAllClubs placeholder function

--- backend/src/controllers/clubController.js
+++ backend/src/controllers/clubController.js
@@ -2,3 +2,7 @@ const db = require('../db');
 
 // Placeholder for future functions
 module.exports = {};
+
+exports.getAllClubs = async (req, res) => {
+  res.json({ message: 'Get all clubs - not implemented yet' });
+};
