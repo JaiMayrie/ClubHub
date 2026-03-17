b5c5ec46e04ac4baa0ee4e1e349f23565afe3058
Shreya <shreyasagar0918@gmail.com>
2026-03-17 19:45:36 -0400

    feat: add GET /api/clubs route definition

--- backend/src/routes/club.js
+++ backend/src/routes/club.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const clubController = require('../controllers/clubController');
+
+// GET all clubs
+router.get('/', clubController.getAllClubs);
 
 module.exports = router;
