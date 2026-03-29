d3f6e900082141d083e31ccc17904937ba1b5d53
Shreya <shreyasagar0918@gmail.com>
2026-03-29 02:38:24 -0400

    config: configure jest coverage thresholds at 30%

--- backend/jest.integration.config.js
+++ backend/jest.integration.config.js
@@ -5,4 +5,19 @@ module.exports = {
   globalTeardown: "<rootDir>/tests/jest.teardown-db.js",
   testMatch: ["<rootDir>/tests/integration/**/*.integration.test.js"],
   testTimeout: 30000,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/controllers/**/*.js",
+    "src/middleware/**/*.js",
+    "src/routes/**/*.js",
+  ],
+  coverageThreshold: {
+    global: {
+      lines: 30,
+      functions: 30,
+      branches: 30,
+      statements: 30,
+    },
+  },
+  coverageReporters: ["text", "lcov"],
 };
